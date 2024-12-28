773edc9d4c0bbcbac008528fef6890f51bca6d68
Edgar Zea <cesszea@icloud.com>
2024-12-28 14:27:56 -0600

    feat(typography): load font and custom text component

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,15 +2,33 @@ import { useEffect } from 'react';
 import * as SplashScreen from 'expo-splash-screen';
 import 'react-native-reanimated';
 import { Slot } from 'expo-router';
+import {
+  RobotoMono_700Bold,
+  RobotoMono_500Medium,
+  RobotoMono_400Regular,
+  useFonts,
+} from '@expo-google-fonts/roboto-mono';
 import { SessionProvider } from '@/contexts/session';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 const RootLayout = () => {
+  const [loaded, error] = useFonts({
+    RobotoMono_700Bold,
+    RobotoMono_500Medium,
+    RobotoMono_400Regular,
+  });
+
   useEffect(() => {
-    SplashScreen.hideAsync();
-  }, []);
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
 
   return (
     <SessionProvider>
---
+++ constants/Fonts.ts
@@ -0,0 +1,5 @@
+export const Fonts = {
+  RobotoBold: 'RobotoMono_700Bold',
+  RobotoMedium: 'RobotoMono_500Medium',
+  RobotoRegular: 'RobotoMono_400Regular',
+}
